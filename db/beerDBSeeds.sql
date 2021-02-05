a67b22e9edfb4c896795eaba7bd7312f14a93a33
Shannon L Johnson-Martin <shannonljohnson-martin@Shannons-MBP.attlocal.net>
2021-02-04 23:04:52 -0500

    Add to seeds file Shannon beer selections

--- db/beerDBSeeds.sql
+++ db/beerDBSeeds.sql
@@ -1,22 +1,61 @@
 USE beer_db;
 
--- There are 20 potential beers to add to the database. Please provide names.
+--There are 20 potential beers to add to the database. 
+--Please provide names.
+--There are 4 of us so each person can provide 5 beer names.--
+
+----------------------------------------------------------------
 
 INSERT INTO beers(beer_name, ordered)
-VALUES("", false);
-
+VALUES("Yoga Pants", false);
+--Beer information: 
+--Style: Southern-Style Pale Ale
+--Alcohol Content: 5.0%ABV
+--Malts: Pilsner, Wheat
+--Hops: Mosaic, Nelson Sauvin, Idaho 7
+--Taste: Not to bitter, sophisticated malt profile and brazen hoppiness
+--Pairing: Pairs with Chicken Sandwiches or Stuffed Mushrooms
+----------------------------------------------------------------
 INSERT INTO beers(beer_name, ordered)
-VALUES("", false);
-
+VALUES("Wacky Wit", false);
+--Beer information: 
+--Style: Belgian-Style Wheat Ale
+--Alcohol Content: 4.1%ABV
+--Malts: Pilsner, Wheat
+--Hops: Czech Saaz
+--Taste: Light and exotic, wheat ale spice with Curacao bitter orange peel, coriander seed and chamomile
+--Pairing: Pairs with Shrimp dishes or Chocolate Moose Cake
+----------------------------------------------------------------
 INSERT INTO beers(beer_name, ordered)
-VALUES("", false);
-
+VALUES("Swamp Fox", false);
+--Beer information: 
+--Style: American-Style IPA
+--Alcohol Content: 7.0%ABV
+--Malts: Irish 2-Row
+--Hops: Centennial, Citra, & Chinook
+--Taste: Assertive bitterness and huge floral, citrus, aroma
+--Pairing: Pairs with Buffalo wings or Creole dishes with chicken and sausage
+----------------------------------------------------------------
 INSERT INTO beers(beer_name, ordered)
-VALUES("", false);
-
+VALUES("The Captains Porter", false);
+--Beer information: 
+--Style: Robust Porter
+--Alcohol Content: 6.0%ABV
+--Malts: Irish 2-Row, CaraMalt, Pale Chocolate
+--Hops: Mt. Hood
+--Taste: Full bodied, semi-sweet dark ale with hints of caramel and chocolate
+--Pairing: Pairs with Pork Shoulder or Baby Back Ribs
+----------------------------------------------------------------
 INSERT INTO beers(beer_name, ordered)
-VALUES("", false);
-
+VALUES("Depravity or Divinity", false);
+--Beer information: 
+--Style: Abbey-Style Dubbel
+--Alcohol Content: 7.8%ABV
+--Malts: Pilsner
+--Hops: Czech Saaz
+--Taste: With hints of raisin, plum, soft caramel, vanilla, and fruity yeast esters
+--Pairing: Pairs with Filet Mignon
+----------------------------------------------------------------
 INSERT INTO beers(beer_name, ordered)
 VALUES("", false);
 
