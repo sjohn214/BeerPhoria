ba4149fb4944e6a9a75688ce4a37629035435fab
Shannon L Johnson-Martin <shannonljohnson-martin@Shannons-MBP.attlocal.net>
2021-02-05 13:15:35 -0500

    Add new column info to seed file

--- db/beerDBSeeds.sql
+++ db/beerDBSeeds.sql
@@ -1,10 +1,5 @@
 USE beer_db;
 
---There are 20 potential beers to add to the database. 
---Please provide names.
---There are 4 of us so each person can provide 5 beer names.--
-
-----------------------------------------------------------------
 
 INSERT INTO beers(beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price, ordered)
 VALUES("Yoga Pants", "Souther-Style Pale Ale", "5.0%ABV", "Pilsner, Wheat", "Mosaic, Nelson, Sauvin, Idaho7", "Not to bitter, sophisticated","Chicken & Stuffed Mushroms", 7, false);
@@ -18,7 +13,7 @@ VALUES("Yoga Pants", "Souther-Style Pale Ale", "5.0%ABV", "Pilsner, Wheat", "Mos
 --Price: $7
 ----------------------------------------------------------------
 INSERT INTO beers(beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price, ordered)
-VALUES("Wacky Wit", "", "", "", "", "", "",  , false);
+VALUES("Wacky Wit", "Belgian-Style Wheat Ale", "4.1%ABV", "Pilsner, Wheat", "Czech Saaz", "Light and exotic", "Shrimp dishes or Chocolate Moose Cake",  7, false);
 --Beer information: 
 --Style: Belgian-Style Wheat Ale
 --Alcohol Content: 4.1%ABV
@@ -29,18 +24,18 @@ VALUES("Wacky Wit", "", "", "", "", "", "",  , false);
 --Price: $7
 ----------------------------------------------------------------
 INSERT INTO beers(beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price, ordered)
-VALUES("Swamp Fox", false);
+VALUES("Swamp Fox", "American-Style IPA", "7.0%ABV", "Irish 2-Row", "Centennial, Citra, & Chinook", "Assertively bitter and floral", "Buffalo wings or Creole foods",  7, false);
 --Beer information: 
 --Style: American-Style IPA
 --Alcohol Content: 7.0%ABV
 --Malts: Irish 2-Row
 --Hops: Centennial, Citra, & Chinook
---Taste: Assertive bitterness and huge floral, citrus, aroma
+--Taste: Assertively bitter and floral, citrus, aroma
 --Pairing: Pairs with Buffalo wings or Creole dishes with chicken and sausage
 --Price: $7
 ----------------------------------------------------------------
 INSERT INTO beers(beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price, ordered)
-VALUES("The Captains Porter", false);
+VALUES("The Captains Porter", "Robust Porter", "6.0%ABV", "Irish 2-Row, CaraMalt, Pale Chocolate", "Mt. Hood", "Full bodied, semi-sweet", "Pork Shoulder or Baby Back Ribs", 7, false);
 --Beer information: 
 --Style: Robust Porter
 --Alcohol Content: 6.0%ABV
@@ -51,57 +46,57 @@ VALUES("The Captains Porter", false);
 --Price: $7
 ----------------------------------------------------------------
 INSERT INTO beers(beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price, ordered)
-VALUES("Depravity or Divinity", 9, false);
+VALUES("Depravity or Divinity", "Abbey-Style Dubbel", "7.8%ABV", "Pilsner", "Czech Saaz", "Lightly sweetened and subtle", "Filet Mignon", 9, false);
 --Beer information: 
 --Style: Abbey-Style Dubbel
 --Alcohol Content: 7.8%ABV
 --Malts: Pilsner
 --Hops: Czech Saaz
---Taste: With hints of raisin, plum, soft caramel, vanilla, and fruity yeast esters
+--Taste: Lightly sweetened and subtle with hints of raisin, plum, soft caramel, vanilla, and fruity yeast esters
 --Pairing: Pairs with Filet Mignon
 --Price: $9
 ----------------------------------------------------------------
 INSERT INTO beers(beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price, ordered)
-VALUES("", false);
+VALUES("", "", "", "", "", "", "",  , false);
 
 INSERT INTO beers(beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price, ordered)
-VALUES("", false);
+VALUES("", "", "", "", "", "", "", , false);
 
 INSERT INTO beers(beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price, ordered)
-VALUES("", false);
+VALUES("", "", "", "", "", "", "", , false);
 
 INSERT INTO beers(beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price, ordered)
-VALUES("", false);
+VALUES("", "", "", "", "", "", "", , false);
 
 INSERT INTO beers(beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price, ordered)
-VALUES("", false);
+VALUES("", "", "", "", "", "", "", , false);
 
 INSERT INTO beers(beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price, ordered)
-VALUES("", false);
+VALUES("", "", "", "", "", "", "", , false);
 
 INSERT INTO beers(beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price, ordered)
-VALUES("", false);
+VALUES("", "", "", "", "", "", "", , false);
 
 INSERT INTO beers(beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price, ordered)
-VALUES("", false);
+VALUES("", "", "", "", "", "", "", , false);
 
 INSERT INTO beers(beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price, ordered)
-VALUES("", false);
+VALUES("", "", "", "", "", "", "", , false);
 
 INSERT INTO beers(beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price, ordered)
-VALUES("", false);
+VALUES("", "", "", "", "", "", "",  , false);
 
 INSERT INTO beers(beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price, ordered)
-VALUES("", false);
+VALUES("", "", "", "", "", "", "", , false);
 
 INSERT INTO beers(beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price, ordered)
-VALUES("", false);
+VALUES("", "", "", "", "", "", "", , false);
 
 INSERT INTO beers(beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price, ordered)
-VALUES("", false);
+VALUES("", "", "", "", "", "", "", , false);
 
 INSERT INTO beers(beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price, ordered)
-VALUES("", false);
+VALUES("", "", "", "", "", "", "", , false);
 
 INSERT INTO beers(beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price, ordered)
-VALUES("", false);
+VALUES("", "", "", "", "", "", "", , false);
