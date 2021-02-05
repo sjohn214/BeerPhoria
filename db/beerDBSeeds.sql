efd45055fb565a87a149ac8355650d11de3af25d
Shannon L Johnson-Martin <shannonljohnson-martin@Shannons-MBP.attlocal.net>
2021-02-05 18:34:25 -0500

    Add new column info to seed file

--- db/beerDBSeeds.sql
+++ db/beerDBSeeds.sql
@@ -57,46 +57,167 @@ VALUES("Depravity or Divinity", "Abbey-Style Dubbel", "7.8%ABV", "Pilsner", "Cze
 --Price: $9
 ----------------------------------------------------------------
 INSERT INTO beers(beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price, ordered)
-VALUES("", "", "", "", "", "", "",  , false);
-
+VALUES("What the Fizz?", "Hard Seltzer", "5.5%ABV", "null", "null", "Loaded with fruit flavor", "Universal",  6, false);
+--Beer information: 
+--Style: Hard Seltzer
+--Alcohol Content: 5.5%ABV
+--Malts: null
+--Hops: null
+--Taste:  Peach and raspberry fruit loaded with flavor
+--Pairing: Pairs with any dish
+--Price: $6
+----------------------------------------------------------------
+INSERT INTO beers(beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price, ordered)
+VALUES("Fizzics", "Hard Seltzer", "5.0%ABV", "null", "null", "Refreshing and light", "Universal", 5, false);
+--Beer information: 
+--Style: Hard Seltzer
+--Alcohol Content: 5.0%ABV
+--Malts: null
+--Hops: null
+--Taste: Refreshing with hints of Meyers lemon and marshmallow
+--Pairing: Pairs with any dish
+--Price: $5
+----------------------------------------------------------------
+INSERT INTO beers(beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price, ordered)
+VALUES("Filling in the Blancs", "Czech-Style", "4.9%ABV", "Pilsner", "Mandarina Bavaria, CTZ, Hallertau Blanc", "Crisp with citrus and herbal aroma", "Universal", 5, false);
+--Beer information: 
+--Style: Czech-Style
+--Alcohol Content: 4.9%ABV
+--Malts: Pilsner
+--Hops: Mandarina Bavaria, CTZ, Hallertau Blanc
+--Taste: Crisp with citrus and herbal aroma
+--Pairing: Pairs with any dish
+--Price: $5
+----------------------------------------------------------------
+INSERT INTO beers(beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price, ordered)
+VALUES("Lovin' the Dream", "American-Style IPA", "5.9%ABV", "null", "Simcoe, Centennial, Cascade", "Lingering subtle bitterness", "Wings or Burgers", 6, false);
+--Beer information: 
+--Style: American-Style IPA
+--Alcohol Content: 5.9%ABV
+--Malts: null
+--Hops: Simcoe, Centennial, Cascade
+--Taste: Lingering subtle bitterness
+--Pairing: Pairs with Wings or Burgers
+--Price: $6
+----------------------------------------------------------------
+INSERT INTO beers(beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price, ordered)
+VALUES("Pacific Thunder", "New England-Style IPA", "6.8%ABV", "null", "Citra, Strata, Nelson", "Piney bitterness with fruitiness", "Wings", 7, false);
+--Beer information: 
+--Style: New England-Style IPA
+--Alcohol Content: 6.8%ABV
+--Malts: null
+--Hops: Citra, Strata, Nelson
+--Taste: Piney bitterness with fruitiness
+--Pairing: Pairs with Wings
+--Price: $7
+----------------------------------------------------------------
+INSERT INTO beers(beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price, ordered)
+VALUES("Long Night", "Spiced Winter-Style Ale", "6.9%ABV", "Irish IPA CaraMalt, Special Roast", "Cascade", "Lightly sweetened and subtle bitterness", "Stuffed Mushrooms or Desserts", 8, false);
+--Beer information: 
+--Style: Spiced Winter-Style Ale
+--Alcohol Content: 6.9%ABV
+--Malts: Irish IPA CaraMalt, Special Roast
+--Hops: Cascade
+--Taste: Lightly sweetened and subtle bitterness
+--Pairing: Pairs with Stuffed Mushrooms or Desserts
+--Price: $8
+----------------------------------------------------------------
+INSERT INTO beers(beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price, ordered)
+VALUES("Dixie Kristal", "Belgian-Style Tripel", "9.3%ABV", "German Pilsner", "Czech Saaz", "Subtle spice and yeast with fruit aroma", "Creole Chicken and Sausage dishes or Filet Mignon", 8, false);
+--Beer information: 
+--Style: Belgian-Style Tripel
+--Alcohol Content: 9.3%ABV
+--Malts: German Pilsner
+--Hops: Czech Saaz
+--Taste: Subtle spice and yeast with fruit aroma
+--Pairing: Pairs with Creole Chicken and Sausage dishes or Filet Mignon
+--Price: $8
+----------------------------------------------------------------
+INSERT INTO beers(beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price, ordered)
+VALUES("Thoughts & Prayers", "Wild Abbey-Style Ale", "8.0%ABV", "Pilsner, Special B, CaraRed", "Czech Saaz", "Lightly acidic blend with wild aromatics", "Dips and Appetizers", 8, false);
+--Beer information: 
+--Style: Wild Abbey-Style Ale
+--Alcohol Content: 8.0%ABV
+--Malts: Pilsner, Special B, CaraRed
+--Hops: Czech Saaz
+--Taste: Lightly acidic blend with wild aromatics
+--Pairing: Pairs with Dips and Appetizers
+--Price: $8
+----------------------------------------------------------------
+INSERT INTO beers(beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price, ordered)
+VALUES("Cellar Dweller", "Barrel-Age", "10.0%ABV", "ale 2-Row, Wheat, Golden Oats, Crystal", "Warrior", "Smooth malt with rich coffee and dark chocolate", "Baby Back Ribs", 10, false);
+--Beer information: 
+--Style: Barrel-Age Imperial Amber Stout-Style
+--Alcohol Content: 10.0%ABV
+--Malts: Pale 2-Row, Wheat, Golden Oats, Crystal
+--Hops: Warrior
+--Taste: Smooth malt with rich coffee and dark chocolate
+--Pairing: Pairs with Baby Back Ribs
+--Price: $10
+----------------------------------------------------------------
+INSERT INTO beers(beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price, ordered)
+VALUES("O, Tannenbaum!", "American-Style IPA", "6.8%ABV", "Irish IPA, Munich", "Chinook, Simcoe", "Festively fermented piney aroma", "Creole dishes with chicken and sausage",  9, false);
+--Beer information: 
+--Style: American-Style IPA
+--Alcohol Content: 6.8%ABV
+--Malts: Irish IPA, Munich
+--Hops: Chinook, Simcoe
+--Taste: Festively fermented piney aroma
+--Pairing: Pairs Creole dishes with chicken and sausage
+--Price: $9
+----------------------------------------------------------------
+INSERT INTO beers(beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price, ordered)
+VALUES("Strange Clouds", "New England-Style IPA", "7.1%ABV", "null", "Mosaic, El Dorado", "Dank with notes of citrus", "Chicken Sandwiches or Stuffed Mushrooms", 6, false);
+--Beer information: 
+--Style: New England-Style IPA
+--Alcohol Content: 7.1%ABV
+--Malts: null
+--Hops: Mosaic, El Dorado
+--Taste: Dank with notes of citrus 
+--Pairing: Pairs with Chicken Sandwiches or Stuffed Mushrooms
+--Price: $6
+----------------------------------------------------------------
+INSERT INTO beers(beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price, ordered)
+VALUES("Hive Fives", "New England-Style Pale Ale", "5.5%ABV", "null", "Citra, Azacca", "Citrusy with subtle sweetness", "Wings and Chicken dishes", 6, false);
+--Beer information: 
+--Style: New England-Style Pale Ale
+--Alcohol Content: 5.5%ABV
+--Malts: null
+--Hops: Citra, Azacca
+--Taste: Citrusy with subtle sweetness
+--Pairing: Pairs with Wings and Chicken dishes
+--Price: $6
+----------------------------------------------------------------
 INSERT INTO beers(beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price, ordered)
 VALUES("", "", "", "", "", "", "", , false);
-
+--Beer information: 
+--Style: Abbey-Style Dubbel
+--Alcohol Content: 7.8%ABV
+--Malts: Pilsner
+--Hops: Czech Saaz
+--Taste: Lightly sweetened and subtle with hints of raisin, plum, soft caramel, vanilla, and fruity yeast esters
+--Pairing: Pairs with Filet Mignon
+--Price: $9
+----------------------------------------------------------------
 INSERT INTO beers(beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price, ordered)
-VALUES("", "", "", "", "", "", "", , false);
-
+VALUES("Global Happiness", "Coffee Brown-Style Ale", "4.8%ABV", "null", "null", "Blended coffee and beer", "Roasted Pork and Smoked Sausage", 6, false);
+--Beer information: 
+--Style: Coffee Brown-Style Ale
+--Alcohol Content: 4.8%ABV
+--Malts: null
+--Hops: null
+--Taste: Blended coffee and beer
+--Pairing: Pairs with Roasted Pork and Smoked Sausage
+--Price: $6
+----------------------------------------------------------------
 INSERT INTO beers(beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price, ordered)
-VALUES("", "", "", "", "", "", "", , false);
-
-INSERT INTO beers(beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price, ordered)
-VALUES("", "", "", "", "", "", "", , false);
-
-INSERT INTO beers(beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price, ordered)
-VALUES("", "", "", "", "", "", "", , false);
-
-INSERT INTO beers(beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price, ordered)
-VALUES("", "", "", "", "", "", "", , false);
-
-INSERT INTO beers(beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price, ordered)
-VALUES("", "", "", "", "", "", "", , false);
-
-INSERT INTO beers(beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price, ordered)
-VALUES("", "", "", "", "", "", "", , false);
-
-INSERT INTO beers(beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price, ordered)
-VALUES("", "", "", "", "", "", "",  , false);
-
-INSERT INTO beers(beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price, ordered)
-VALUES("", "", "", "", "", "", "", , false);
-
-INSERT INTO beers(beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price, ordered)
-VALUES("", "", "", "", "", "", "", , false);
-
-INSERT INTO beers(beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price, ordered)
-VALUES("", "", "", "", "", "", "", , false);
-
-INSERT INTO beers(beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price, ordered)
-VALUES("", "", "", "", "", "", "", , false);
-
-INSERT INTO beers(beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price, ordered)
-VALUES("", "", "", "", "", "", "", , false);
+VALUES("Tu Bock's Back", "Dunkelweizen-Style", "8.7%ABV", "null", "null", "Boldly sweetened with toffee and caramelized banana", "Dishes that have Savory Sauces", 7, false);
+--Beer information: 
+--Style: Dunkelweizen-Style 
+--Alcohol Content: 8.7%ABV
+--Malts: null
+--Hops: null
+--Taste: Boldly sweetened with toffee and caramelized banana
+--Pairing: Pairs with Dishes that have Savory Sauces
+--Price: $7
+----------------------------------------------------------------
