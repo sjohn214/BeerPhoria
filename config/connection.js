4774ec92cf338bf3dbd302ca3fb10628a4db30db
Shannon L Johnson-Martin <shannonljohnson-martin@Shannons-MBP.attlocal.net>
2021-02-05 19:17:11 -0500

    jawsdb connection

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ if(process.env.JAWSDB_URL) {
 connection = mysql.createConnection(process.env.JAWSDB_URL);
 }else{
     connection = mysql.createConnection({
-    host: 'localhost',
+    host: 'localhostproject2',
     port: 3306,
     user: 'root',
     password: 'rootingABC1',
