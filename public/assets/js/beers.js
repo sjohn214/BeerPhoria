95458c21f80a173f252cf6adb7d8be55138cfca0
Shannon L Johnson-Martin <shannonljohnson-martin@Shannons-MBP.attlocal.net>
2021-02-12 10:17:08 -0500

    Add browserify npm package to allow require statements on client side

--- public/assets/js/beers.js
+++ public/assets/js/beers.js
@@ -1,21 +1,21 @@
-const { search, suggest, regex } = require('puzzy-search');
 
-var  str = document.querySelector('#search');
-var  btnSearch = document.querySelector('#btnSearch');
-var  suggestion = document.querySelector('#didyoumean');
-var  resultList = document.querySelector('#result');
-str.addEventListener('input', () => {
-    if (str.value)
-        suggestion.innerHTML = `Did you mean: ${suggest(str.value)}`
-    else
-        suggestion.innerHTML = ''
-});
+const { search, regex, suggest } = require('puzzy-search')
 
-btnSearch.addEventListener('click', () => {
-    let  regex = regex(str.value)
-    fetch('api.php?regex=' + regex)
-        .then(res  =>  res.json()).then(data  => {
-            resultList.innerHTML = data.map(r  =>  `<li>${r.sampleName}</li>`).join('')
-        }).catch(e  =>  console.log(e))
-});
-
+    var  str = document.querySelector('#search')
+    var  btnSearch = document.querySelector('#btnSearch')
+    var  suggestion = document.querySelector('#didyoumean')
+    var  resultList = document.querySelector('#result')
+    str.addEventListener('input', () => {
+        if (search(str.value))
+            suggestion.innerHTML = `Did you mean: ${suggest(str.value)}`
+        else
+            suggestion.innerHTML = ''
+    });
+    
+    btnSearch.addEventListener('click', () => {
+        regex = regex(str.value)
+        fetch('api.php?regex=' + regex)
+            .then(res  =>  res.json()).then(data  => {
+                resultList.innerHTML = data.map(r  =>  `<li>${r.sampleName}</li>`).join('')
+            }).catch(e  =>  console.log(e))
+    });
