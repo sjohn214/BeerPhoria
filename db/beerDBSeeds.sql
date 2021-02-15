7e0e59b9203aaf07532ebd4acd34d2b2c9360f01
Shannon L Johnson-Martin <shannonljohnson-martin@Shannons-MBP.attlocal.net>
2021-02-15 16:59:13 -0500

    Add resolved seed info

--- db/beerDBSeeds.sql
+++ db/beerDBSeeds.sql
@@ -1,8 +1,8 @@
 USE beer_db;
 
 
-INSERT INTO beers(brewery_name, beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price, ordered)
-VALUES("Moon River Brewing Company", "Yoga Pants", "Souther-Style Pale Ale", 5, "Pilsner, Wheat", "Mosaic, Nelson, Sauvin, Idaho7", "Not to bitter, sophisticated","Chicken & Stuffed Mushroms", 7, false);
+INSERT INTO beers(brewery_name, beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price)
+VALUES("Moon River Brewing Company", "Yoga Pants", "Souther-Style Pale Ale", 5, "Pilsner, Wheat", "Mosaic, Nelson, Sauvin, Idaho7", "Not to bitter, sophisticated","Chicken & Stuffed Mushroms", 7);
 -- --Beer information: 
 -- --Style: Southern-Style Pale Ale
 -- --Alcohol Content: 5.0%ABV
@@ -12,8 +12,8 @@ VALUES("Moon River Brewing Company", "Yoga Pants", "Souther-Style Pale Ale", 5, 
 -- --Pairing: Pairs with Chicken Sandwiches or Stuffed Mushrooms
 -- --Price: $7
 -- ----------------------------------------------------------------
-INSERT INTO beers(brewery_name, beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price, ordered)
-VALUES("Moon River Brewing Company", "Wacky Wit", "Belgian-Style Wheat Ale", 4.1, "Pilsner, Wheat", "Czech Saaz", "Light and exotic", "Shrimp dishes or Chocolate Moose Cake",  7, false);
+INSERT INTO beers(brewery_name, beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price)
+VALUES("Moon River Brewing Company", "Wacky Wit", "Belgian-Style Wheat Ale", 4.1, "Pilsner, Wheat", "Czech Saaz", "Light and exotic", "Shrimp dishes or Chocolate Moose Cake",  7);
 -- --Beer information: 
 -- --Style: Belgian-Style Wheat Ale
 -- --Alcohol Content: 4.1%ABV
@@ -23,8 +23,8 @@ VALUES("Moon River Brewing Company", "Wacky Wit", "Belgian-Style Wheat Ale", 4.1
 -- --Pairing: Pairs with Shrimp dishes or Chocolate Moose Cake
 -- --Price: $7
 -- ----------------------------------------------------------------
-INSERT INTO beers(brewery_name, beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price, ordered)
-VALUES("Moon River Brewing Company", "Swamp Fox", "American-Style IPA", 7, "Irish 2-Row", "Centennial, Citra, & Chinook", "Assertively bitter and floral", "Buffalo wings or Creole foods",  7, false);
+INSERT INTO beers(brewery_name, beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price)
+VALUES("Moon River Brewing Company", "Swamp Fox", "American-Style IPA", 7, "Irish 2-Row", "Centennial, Citra, & Chinook", "Assertively bitter and floral", "Buffalo wings or Creole foods",  7);
 -- --Beer information: 
 -- --Style: American-Style IPA
 -- --Alcohol Content: 7.0%ABV
@@ -34,8 +34,8 @@ VALUES("Moon River Brewing Company", "Swamp Fox", "American-Style IPA", 7, "Iris
 -- --Pairing: Pairs with Buffalo wings or Creole dishes with chicken and sausage
 -- --Price: $7
 -- ----------------------------------------------------------------
-INSERT INTO beers(brewery_name, beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price, ordered)
-VALUES("Moon River Brewing Company", "The Captains Porter", "Robust Porter", 6, "Irish 2-Row, CaraMalt, Pale Chocolate", "Mt. Hood", "Full bodied, semi-sweet", "Pork Shoulder or Baby Back Ribs", 7, false);
+INSERT INTO beers(brewery_name, beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price)
+VALUES("Moon River Brewing Company", "The Captains Porter", "Robust Porter", 6, "Irish 2-Row, CaraMalt, Pale Chocolate", "Mt. Hood", "Full bodied, semi-sweet", "Pork Shoulder or Baby Back Ribs", 7);
 -- --Beer information: 
 -- --Style: Robust Porter
 -- --Alcohol Content: 6.0%ABV
@@ -45,8 +45,8 @@ VALUES("Moon River Brewing Company", "The Captains Porter", "Robust Porter", 6, 
 -- --Pairing: Pairs with Pork Shoulder or Baby Back Ribs
 -- --Price: $7
 -- ----------------------------------------------------------------
-INSERT INTO beers(brewery_name, beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price, ordered)
-VALUES("Moon River Brewing Company", "Depravity or Divinity", "Abbey-Style Dubbel", 7.8, "Pilsner", "Czech Saaz", "Lightly sweetened and subtle", "Filet Mignon", 9, false);
+INSERT INTO beers(brewery_name, beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price)
+VALUES("Moon River Brewing Company", "Depravity or Divinity", "Abbey-Style Dubbel", 7.8, "Pilsner", "Czech Saaz", "Lightly sweetened and subtle", "Filet Mignon", 9);
 -- --Beer information: 
 -- --Style: Abbey-Style Dubbel
 -- --Alcohol Content: 7.8%ABV
@@ -56,8 +56,8 @@ VALUES("Moon River Brewing Company", "Depravity or Divinity", "Abbey-Style Dubbe
 -- --Pairing: Pairs with Filet Mignon
 -- --Price: $9
 -- ----------------------------------------------------------------
-INSERT INTO beers(brewery_name, beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price, ordered)
-VALUES("Heist Brewery", "What the Fizz?", "Hard Seltzer", 5.5, "null", "null", "Loaded with fruit flavor", "Universal",  6, false);
+INSERT INTO beers(brewery_name, beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price)
+VALUES("Heist Brewery", "What the Fizz?", "Hard Seltzer", 5.5, "null", "null", "Loaded with fruit flavor", "Universal",  6);
 -- --Beer information: 
 -- --Style: Hard Seltzer
 -- --Alcohol Content: 5.5%ABV
@@ -67,8 +67,8 @@ VALUES("Heist Brewery", "What the Fizz?", "Hard Seltzer", 5.5, "null", "null", "
 -- --Pairing: Pairs with any dish
 -- --Price: $6
 -- ----------------------------------------------------------------
-INSERT INTO beers(brewery_name, beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price, ordered)
-VALUES("Heist Brewery", "Fizzics", "Hard Seltzer", 5, "null", "null", "Refreshing and light", "Universal", 5, false);
+INSERT INTO beers(brewery_name, beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price)
+VALUES("Heist Brewery", "Fizzics", "Hard Seltzer", 5, "null", "null", "Refreshing and light", "Universal", 5);
 -- --Beer information: 
 -- --Style: Hard Seltzer
 -- --Alcohol Content: 5.0%ABV
@@ -78,8 +78,8 @@ VALUES("Heist Brewery", "Fizzics", "Hard Seltzer", 5, "null", "null", "Refreshin
 -- --Pairing: Pairs with any dish
 -- --Price: $5
 -- ----------------------------------------------------------------
-INSERT INTO beers(brewery_name, beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price, ordered)
-VALUES("Heist Brewery", "Filling in the Blancs", "Czech-Style", 4.9, "Pilsner", "Mandarina Bavaria, CTZ, Hallertau Blanc", "Crisp with citrus and herbal aroma", "Universal", 5, false);
+INSERT INTO beers(brewery_name, beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price)
+VALUES("Heist Brewery", "Filling in the Blancs", "Czech-Style", 4.9, "Pilsner", "Mandarina Bavaria, CTZ, Hallertau Blanc", "Crisp with citrus and herbal aroma", "Universal", 5);
 -- --Beer information: 
 -- --Style: Czech-Style
 -- --Alcohol Content: 4.9%ABV
@@ -89,8 +89,8 @@ VALUES("Heist Brewery", "Filling in the Blancs", "Czech-Style", 4.9, "Pilsner", 
 -- --Pairing: Pairs with any dish
 -- --Price: $5
 -- ----------------------------------------------------------------
-INSERT INTO beers(brewery_name, beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price, ordered)
-VALUES("Heist Brewery", "Lovin' the Dream", "American-Style IPA", 5.9, "null", "Simcoe, Centennial, Cascade", "Lingering subtle bitterness", "Wings or Burgers", 6, false);
+INSERT INTO beers(brewery_name, beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price)
+VALUES("Heist Brewery", "Lovin' the Dream", "American-Style IPA", 5.9, "null", "Simcoe, Centennial, Cascade", "Lingering subtle bitterness", "Wings or Burgers", 6);
 -- --Beer information: 
 -- --Style: American-Style IPA
 -- --Alcohol Content: 5.9%ABV
@@ -100,8 +100,8 @@ VALUES("Heist Brewery", "Lovin' the Dream", "American-Style IPA", 5.9, "null", "
 -- --Pairing: Pairs with Wings or Burgers
 -- --Price: $6
 -- ----------------------------------------------------------------
-INSERT INTO beers(brewery_name, beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price, ordered)
-VALUES("Heist Brewery", "Pacific Thunder", "New England-Style IPA", 6.8, "null", "Citra, Strata, Nelson", "Piney bitterness with fruitiness", "Wings", 7, false);
+INSERT INTO beers(brewery_name, beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price)
+VALUES("Heist Brewery", "Pacific Thunder", "New England-Style IPA", 6.8, "null", "Citra, Strata, Nelson", "Piney bitterness with fruitiness", "Wings", 7);
 -- --Beer information: 
 -- --Style: New England-Style IPA
 -- --Alcohol Content: 6.8%ABV
@@ -111,8 +111,8 @@ VALUES("Heist Brewery", "Pacific Thunder", "New England-Style IPA", 6.8, "null",
 -- --Pairing: Pairs with Wings
 -- --Price: $7
 -- ----------------------------------------------------------------
-INSERT INTO beers(brewery_name, beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price, ordered)
-VALUES("Moon River Brewing Company Company", "Long Night", "Spiced Winter-Style Ale", 6.9, "Irish IPA CaraMalt, Special Roast", "Cascade", "Lightly sweetened and subtle bitterness", "Stuffed Mushrooms or Desserts", 8, false);
+INSERT INTO beers(brewery_name, beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price)
+VALUES("Moon River Brewing Company Company", "Long Night", "Spiced Winter-Style Ale", 6.9, "Irish IPA CaraMalt, Special Roast", "Cascade", "Lightly sweetened and subtle bitterness", "Stuffed Mushrooms or Desserts", 8);
 -- --Beer information: 
 -- --Style: Spiced Winter-Style Ale
 -- --Alcohol Content: 6.9%ABV
@@ -122,8 +122,8 @@ VALUES("Moon River Brewing Company Company", "Long Night", "Spiced Winter-Style 
 -- --Pairing: Pairs with Stuffed Mushrooms or Desserts
 -- --Price: $8
 -- ----------------------------------------------------------------
-INSERT INTO beers(brewery_name, beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price, ordered)
-VALUES("Moon River Brewing Company Company", "Dixie Kristal", "Belgian-Style Tripel", 9.3, "German Pilsner", "Czech Saaz", "Subtle spice and yeast with fruit aroma", "Creole Chicken and Sausage dishes or Filet Mignon", 8, false);
+INSERT INTO beers(brewery_name, beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price)
+VALUES("Moon River Brewing Company Company", "Dixie Kristal", "Belgian-Style Tripel", 9.3, "German Pilsner", "Czech Saaz", "Subtle spice and yeast with fruit aroma", "Creole Chicken and Sausage dishes or Filet Mignon", 8);
 -- --Beer information: 
 -- --Style: Belgian-Style Tripel
 -- --Alcohol Content: 9.3%ABV
@@ -133,8 +133,8 @@ VALUES("Moon River Brewing Company Company", "Dixie Kristal", "Belgian-Style Tri
 -- --Pairing: Pairs with Creole Chicken and Sausage dishes or Filet Mignon
 -- --Price: $8
 -- ----------------------------------------------------------------
-INSERT INTO beers(brewery_name, beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price, ordered)
-VALUES("Moon River Brewing Company", "Thoughts & Prayers", "Wild Abbey-Style Ale", 8, "Pilsner, Special B, CaraRed", "Czech Saaz", "Lightly acidic blend with wild aromatics", "Dips and Appetizers", 8, false);
+INSERT INTO beers(brewery_name, beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price)
+VALUES("Moon River Brewing Company", "Thoughts & Prayers", "Wild Abbey-Style Ale", 8, "Pilsner, Special B, CaraRed", "Czech Saaz", "Lightly acidic blend with wild aromatics", "Dips and Appetizers", 8);
 -- --Beer information: 
 -- --Style: Wild Abbey-Style Ale
 -- --Alcohol Content: 8.0%ABV
@@ -144,8 +144,8 @@ VALUES("Moon River Brewing Company", "Thoughts & Prayers", "Wild Abbey-Style Ale
 -- --Pairing: Pairs with Dips and Appetizers
 -- --Price: $8
 -- ----------------------------------------------------------------
-INSERT INTO beers(brewery_name, beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price, ordered)
-VALUES("Moon River Brewing Company", "Cellar Dweller", "Barrel-Age", 10, "Pale 2-Row, Wheat, Golden Oats, Crystal", "Warrior", "Smooth malt with rich coffee and dark chocolate", "Baby Back Ribs", 10, false);
+INSERT INTO beers(brewery_name, beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price)
+VALUES("Moon River Brewing Company", "Cellar Dweller", "Barrel-Age", 10, "Pale 2-Row, Wheat, Golden Oats, Crystal", "Warrior", "Smooth malt with rich coffee and dark chocolate", "Baby Back Ribs", 10);
 -- --Beer information: 
 -- --Style: Barrel-Age Imperial Amber Stout-Style
 -- --Alcohol Content: 10.0%ABV
@@ -155,8 +155,8 @@ VALUES("Moon River Brewing Company", "Cellar Dweller", "Barrel-Age", 10, "Pale 2
 -- --Pairing: Pairs with Baby Back Ribs
 -- --Price: $10
 -- ----------------------------------------------------------------
-INSERT INTO beers(brewery_name, beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price, ordered)
-VALUES("Moon River Brewing Company", "O, Tannenbaum!", "American-Style IPA", 6, "Irish IPA, Munich", "Chinook, Simcoe", "Festively fermented piney aroma", "Creole dishes with chicken and sausage",  9, false);
+INSERT INTO beers(brewery_name, beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price)
+VALUES("Moon River Brewing Company", "O, Tannenbaum!", "American-Style IPA", 6, "Irish IPA, Munich", "Chinook, Simcoe", "Festively fermented piney aroma", "Creole dishes with chicken and sausage",  9);
 -- --Beer information: 
 -- --Style: American-Style IPA
 -- --Alcohol Content: 6.8%ABV
@@ -166,8 +166,8 @@ VALUES("Moon River Brewing Company", "O, Tannenbaum!", "American-Style IPA", 6, 
 -- --Pairing: Pairs Creole dishes with chicken and sausage
 -- --Price: $9
 -- ----------------------------------------------------------------
-INSERT INTO beers(brewery_name, beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price, ordered)
-VALUES("Heist Brewery", "Strange Clouds", "New England-Style IPA", 7.1, "null", "Mosaic, El Dorado", "Dank with notes of citrus", "Chicken Sandwiches or Stuffed Mushrooms", 6, false);
+INSERT INTO beers(brewery_name, beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price)
+VALUES("Heist Brewery", "Strange Clouds", "New England-Style IPA", 7.1, "null", "Mosaic, El Dorado", "Dank with notes of citrus", "Chicken Sandwiches or Stuffed Mushrooms", 6);
 -- --Beer information: 
 -- --Style: New England-Style IPA
 -- --Alcohol Content: 7.1%ABV
@@ -177,8 +177,8 @@ VALUES("Heist Brewery", "Strange Clouds", "New England-Style IPA", 7.1, "null", 
 -- --Pairing: Pairs with Chicken Sandwiches or Stuffed Mushrooms
 -- --Price: $6
 -- ----------------------------------------------------------------
-INSERT INTO beers(brewery_name, beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price, ordered)
-VALUES("Heist Brewery", "Hive Fives", "New England-Style Pale Ale", 5.5, "null", "Citra, Azacca", "Citrusy with subtle sweetness", "Wings and Chicken dishes", 6, false);
+INSERT INTO beers(brewery_name, beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price)
+VALUES("Heist Brewery", "Hive Fives", "New England-Style Pale Ale", 5.5, "null", "Citra, Azacca", "Citrusy with subtle sweetness", "Wings and Chicken dishes", 6);
 -- --Beer information: 
 -- --Style: New England-Style Pale Ale
 -- --Alcohol Content: 5.5%ABV
@@ -188,8 +188,8 @@ VALUES("Heist Brewery", "Hive Fives", "New England-Style Pale Ale", 5.5, "null",
 -- --Pairing: Pairs with Wings and Chicken dishes
 -- --Price: $6
 -- ----------------------------------------------------------------
-INSERT INTO beers(brewery_name, beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price, ordered)
-VALUES("Legion Brewing Company", "Penguin Pils", "German-Style", 4.8, "Carolina Pilsner", "Saaz", "Mild floral aroma", "Salmon and Tuna", 8, false);
+INSERT INTO beers(brewery_name, beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price)
+VALUES("Legion Brewing Company", "Penguin Pils", "German-Style", 4.8, "Carolina Pilsner", "Saaz", "Mild floral aroma", "Salmon and Tuna", 8);
 -- --Beer information: 
 -- --Style: German-Style
 -- --Alcohol Content: 4.8%ABV
@@ -199,8 +199,8 @@ VALUES("Legion Brewing Company", "Penguin Pils", "German-Style", 4.8, "Carolina 
 -- --Pairing: Pairs with Salmon and Tuna 
 -- --Price: $8
 -- ----------------------------------------------------------------
-INSERT INTO beers(brewery_name, beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price, ordered)
-VALUES("Heist Brewery", "Global Happiness", "Coffee Brown-Style Ale", 4.8, "null", "null", "Blended coffee and beer", "Roasted Pork and Smoked Sausage", 6, false);
+INSERT INTO beers(brewery_name, beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price)
+VALUES("Heist Brewery", "Global Happiness", "Coffee Brown-Style Ale", 4.8, "null", "null", "Blended coffee and beer", "Roasted Pork and Smoked Sausage", 6);
 -- --Beer information: 
 -- --Style: Coffee Brown-Style Ale
 -- --Alcohol Content: 4.8%ABV
@@ -210,8 +210,8 @@ VALUES("Heist Brewery", "Global Happiness", "Coffee Brown-Style Ale", 4.8, "null
 -- --Pairing: Pairs with Roasted Pork and Smoked Sausage
 -- --Price: $6
 -- ----------------------------------------------------------------
-INSERT INTO beers(brewery_name, beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price, ordered)
-VALUES("Heist Brewery", "Tu Bock's Back", "Dunkelweizen-Style", 8.7, "null", "null", "Boldly sweetened with toffee and caramelized banana", "Dishes that have Savory Sauces", 7, false);
+INSERT INTO beers(brewery_name, beer_name, beer_style, alcohol_content, malt_type, hop_type, beer_taste, beer_pairing, beer_price)
+VALUES("Heist Brewery", "Tu Bock's Back", "Dunkelweizen-Style", 8.7, "null", "null", "Boldly sweetened with toffee and caramelized banana", "Dishes that have Savory Sauces", 7);
 -- --Beer information: 
 -- --Style: Dunkelweizen-Style 
 -- --Alcohol Content: 8.7%ABV
