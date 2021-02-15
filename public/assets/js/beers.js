9a41f53d377fc917c7a26c0e45496d45dee7a12a
JonGudenzi <jongudenzi@gmail.com>
2021-02-14 21:39:38 -0500

    post route

--- public/assets/js/beers.js
+++ public/assets/js/beers.js
@@ -1,44 +1,56 @@
 const { search, suggest, regex } = puzzySearch
 
-    var  str = document.querySelector('#search')
-    var  btnSearch = document.querySelector('#btnSearch')
-    var  suggestion = document.querySelector('#didyoumean')
-    var  resultList = document.querySelector('#result')
+var str = document.querySelector('#search')
+var btnSearch = document.querySelector('#btnSearch')
+var suggestion = document.querySelector('#didyoumean')
+var resultList = document.querySelector('#result')
 
 
- let sentence = 'Searching for beers...? See what we have on tap!'
- 
- search(str.value, sentence) // true
- 
- suggest(str.value, sentence) // popular beers
- 
- regex(str.value) // new RegExp(...puzzy...)
- console.log(sentence.match);
+let sentence = 'Searching for beers...? See what we have on tap!'
+
+search(str.value, sentence) // true
+
+suggest(str.value, sentence) // popular beers
+
+regex(str.value) // new RegExp(...puzzy...)
+console.log(sentence.match);
 sentence.match(regex(str.value)) !== null;
-    str.addEventListener('input', () => {
-        if (search(str.value, sentence))
-            suggestion.innerHTML = `Did you mean: ${suggest(str.value)}`
-        else
-            suggestion.innerHTML = ''
-    });
+str.addEventListener('input', () => {
+    if (search(str.value, sentence))
+        suggestion.innerHTML = `Did you mean: ${suggest(str.value)}`
+    else
+        suggestion.innerHTML = ''
+});
 
-        $("#btnSearch").on("click", function (){
-           // debugger;
-            var searchValue= $("#search").val();
-                //console.log(searchValue);
-        // $.ajax({
-        //     method: 'GET',
-        //     url: '/api/beer?q=' + searchValue,
-        //     success: function(data) {
-        //        console.log(data);
-        //         //  newDiv.push(searchValue);
-        //     }
-        // })
+$("#btnSearch").on("click", function () {
+    var searchValue = $("#search").val();
+    if (searchValue) {
+        location.replace("/beers/" + searchValue);
+    }
+    else {
+        location.replace("/");
+    }
+});
 
-        if (searchValue){
-            location.replace("/beers/"+searchValue);
-        }
-        else{
-            location.replace("/");
-        }
-        } );
+$('.create-btn').on('click', function (event) {
+    event.preventDefault();
+
+    var nameInput = $(this).siblings('[name=beer_name]').val();
+    if (nameInput) {
+      var beerInfo = {
+        beer_name: nameInput,
+      };
+     // console.log(beerInfo);
+      $.ajax({
+        method: 'POST',
+        url: '/api/beers/create',
+        data: beerInfo,
+      }).then(function (data) {
+        // reload page to display devoured burger in proper column
+        location.reload();
+      });
+    }
+  });
+
+
+ 
