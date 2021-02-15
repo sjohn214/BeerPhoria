c4b879fe783a59ab4b52ce69571be38d2609fc93
JonGudenzi <jongudenzi@gmail.com>
2021-02-14 20:48:24 -0500

    post route work to add a new beer to table

--- controllers/beer_controller.js
+++ controllers/beer_controller.js
@@ -2,34 +2,39 @@ var express = require("express");
 var router = express.Router();
 var beer = require("../models/beer.js");
 
-// router.get("/", (req, res) => {
-//   beer.all((data) => {
-//     var hbsObject = {
-//       beers: data
-//     };
-// console.log(hbsObject);
-//     res.render("index", hbsObject);
-//   });
+// router.post("/beers/create", async (req, res) => {
+//   beer.create(["brewery_name", "beer_name", "beer_style", "alcohol_content", "beer_taste", "beer_pairing", "ordered"],
+//     [req.body.name, req.body.false], (result) => {
+//       res.json({ result });
+//     });
 // });
-//////////////////////////////////////////////////////////////////////////
 
-router.post("/beers/create", (req, res) => {
-  beer.create(["brewery_name","beer_name", "beer_style","alcohol_content","beer_taste","beer_pairing","ordered"], 
-  [req.body.name, req.body. false], (result) => {
-    res.json({ result });
+
+router.post('/api/beers/create', async (req, res) => {
+  // edited burger create to add in a burger_name
+  const newBeer = new db.beers({
+    beer_name: req.body.beer_name,
   });
+  try {
+    const dbBeer = await newBeer.save();
+    // redirect
+    res.redirect('/');
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
+
 /////////////////////////////////////////////////////////////////////////
 router.put("/beers/ordered/:id", (req, res) => {
   var condition = "id = " + req.params.id;
 
-  beer.update({ ordered: req.body.ordered},
+  beer.update({ ordered: req.body.ordered },
     condition,
     (result) => {
       res.json(result)
     }
   );
-  console.log("condition",condition);
+  console.log("condition", condition);
 });
 
 // Export routes for server.js to use.
