8016234f5361b2f1ec513c080c756ad68eb1433b
JonGudenzi <jongudenzi@gmail.com>
2021-02-16 17:48:20 -0500

    post routes

--- controllers/beer_controller.js
+++ controllers/beer_controller.js
@@ -1,30 +1,19 @@
 var express = require("express");
 var router = express.Router();
 var beer = require("../models/beer.js");
-
-// router.post("/beers/create", async (req, res) => {
-//   beer.create(["brewery_name", "beer_name", "beer_style", "alcohol_content", "beer_taste", "beer_pairing", "ordered"],
-//     [req.body.name, req.body.false], (result) => {
-//       res.json({ result });
-//     });
-// });
+var db = require("../models");
 
 
+////////////////////   POST   ///////////////////////////////////
 router.post('/api/beers/create', async (req, res) => {
-  // edited burger create to add in a burger_name
-  const newBeer = new db.beers({
+  const newBeer = await db.beer.create({
     beer_name: req.body.beer_name,
   });
-  try {
-    const dbBeer = await newBeer.save();
-    // redirect
-    res.redirect('/');
-  } catch (err) {
-    res.status(500).json(err);
-  }
+  console.log(newBeer);
 });
 
-/////////////////////////////////////////////////////////////////////////
+
+////////////////////   PUT   ///////////////////////////////////
 router.put("/beers/ordered/:id", (req, res) => {
   var condition = "id = " + req.params.id;
 
@@ -34,8 +23,7 @@ router.put("/beers/ordered/:id", (req, res) => {
       res.json(result)
     }
   );
-  console.log("condition", condition);
+  
 });
 
-// Export routes for server.js to use.
 module.exports = router;
--- models/beer.js
+++ models/beer.js
@@ -5,21 +5,21 @@ module.exports = function (sequelize, DataTypes){
 var beer = sequelize.define("beer", {
    brewery_name: {
        type: DataTypes.STRING,
-       allowNull: false,
+       allowNull: true,
        validate: {
            len: [1]
        }
    },
    beer_name: {
         type: DataTypes.STRING,
-        allowNull: false,
+        allowNull: true,
         validate: {
             len: [1]
         }
     },
     beer_style: {
         type: DataTypes.STRING,
-        allowNull: false,
+        allowNull: true,
         validate: {
             len: [1]
         }
@@ -27,32 +27,32 @@ var beer = sequelize.define("beer", {
     alcohol_content: {
         type: DataTypes.DECIMAL(10,2),
         //type: DataTypes.RANGE(DataTypes.DECIMAL) numeric range//
-        allowNull: false,
+        allowNull: true,
     },
     malt_type: {
         type: DataTypes.STRING,
-        allowNull: false,
+        allowNull: true,
         validate: {
             len: [1]
         }
     },
     hop_type: {
         type: DataTypes.STRING,
-        allowNull: false,
+        allowNull: true,
         validate: {
             len: [1]
         }
     },
     beer_taste: {
         type: DataTypes.STRING,
-        allowNull: false,
+        allowNull: true,
         validate: {
             len: [1]
         }
     },
     beer_pairing: {
         type: DataTypes.STRING,
-        allowNull: false,
+        allowNull: true,
         validate: {
             len: [1]
         }
@@ -60,7 +60,7 @@ var beer = sequelize.define("beer", {
     beer_price: {
         type: DataTypes.DECIMAL(10,2),
         //type: DataTypes.RANGE(DataTypes.DECIMAL) numeric range//
-        allowNull: false,
+        allowNull: true,
         
     }
 },{timestamps: false});
